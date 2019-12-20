3b37d8bc994e0da9918bf223335ca6fe66420c95
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-20 20:02:13 +0100

    read/ write models added

--- Logger/Interfaces/IWriter.cs
+++ Logger/Interfaces/IWriter.cs
@@ -6,7 +6,7 @@ namespace Logger.Interfaces
 {
     public interface IWriter
     {
-        string WriteLine(string message);
-        string Write(string message);
+        void WriteLine(string message);
+        void Write(string message);
     }
 }
