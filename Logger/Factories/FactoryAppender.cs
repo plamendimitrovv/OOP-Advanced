a48feb7c24139dfd0a0dd7fa00cde67751f282ee
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-20 16:14:42 +0100

    Factories implemented

---
+++ Logger/Factories/FactoryAppender.cs
@@ -0,0 +1,26 @@
+﻿using Logger.Interfaces;
+using Logger.Models.AppenderModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger.Factories
+{
+    public class FactoryAppender
+    {
+        public IAppender Create(string type, ILayout layout, IWriter writer)
+        {
+            if (type == "ConsoleAppender")
+            {
+                return new ConsoleAppender(layout, writer);
+            }
+
+            else if (type == "FileAppender")
+            {
+                return FileAppender(layout); 
+            }
+
+            throw new ArgumentException("Invalid type of Appender!"); 
+        }
+    }
+}
---
+++ Logger/Factories/FactoryLayout.cs
@@ -0,0 +1,26 @@
+﻿using Logger.Interfaces;
+using Logger.Models.LayoutModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger.Factories
+{
+    public class FactoryLayout
+    {
+        public ILayout Create(string layoutType)
+        {
+            if (layoutType == "SimpleLayout")
+            {
+                return new SimpleLayout(); 
+            }
+
+            else if (layoutType == "XmlLayout")
+            {
+                return new XmlLayout(); 
+            }
+
+            throw new ArgumentException("Invalid type of Layout!");
+        }
+    }
+}
