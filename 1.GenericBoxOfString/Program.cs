33df3fac12439722d6dd31552f41454e265ed333
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-22 22:08:29 +0100

    1.Generic Box of String solution

---
+++ 1.GenericBoxOfString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1.GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberofLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberofLines; i++)
+            {
+                var box = new Box<string>(Console.ReadLine());
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
