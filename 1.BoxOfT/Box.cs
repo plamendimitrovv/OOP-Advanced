c83eb5a34ce59bbc78637539ca1914c60ebd4856
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-22 15:41:33 +0100

    BoxOfT solution

---
+++ 1.BoxOfT/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+   public class Box<T>
+    {
+        private readonly List<T> data; 
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+     //   public IReadOnlyCollection<T> Data => this.data.AsReadOnly();
+        public int Count => this.data.Count;
+
+
+        // Adding should add on top of its contents
+        public void Add(T element)
+        {
+            this.data.Add(element);
+        }
+
+
+        //Remove should get the topmost element
+        public T Remove()
+        {
+            T element = this.data[Count - 1];
+            this.data.RemoveAt(this.data.Count - 1);
+
+            return element;
+        }
+
+
+        // print the remaning elements
+        //public void Print()
+        //{
+        //    foreach (var item in data)
+        //    {
+        //        Console.WriteLine(item.ToString());
+        //    } 
+        //}
+
+    }
+
