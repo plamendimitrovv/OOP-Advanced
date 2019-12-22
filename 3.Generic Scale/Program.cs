2267ea4cd7eb8c2466827dfbad19f3123eb5f031
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-22 18:45:54 +0100

    Generic scale solution

---
+++ 3.Generic Scale/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generic_Scale
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var scale = new Scale<int>(18, 8);
+            var scale2 = new Scale<string>("a2bd", "abd");
+            var scale3 = new Scale<double>(1.01, 0.5);
+
+            Console.WriteLine(scale.GetHeavier());
+            Console.WriteLine(scale2.GetHeavier());
+            Console.WriteLine(scale3.GetHeavier());
+
+
+        }
+    }
+}
