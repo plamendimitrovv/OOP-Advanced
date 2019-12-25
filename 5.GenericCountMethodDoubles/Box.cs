8cdad4af4dcd0dff156d668a70240797da8a1abf
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-25 14:57:59 +0100

    6.GenericCountMethodDoubles Solution

---
+++ 5.GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6.GenericCountMethodDoubles
+{
+    public class Box<T>
+        where T : IComparable<T>
+    {
+
+        public T Value { get; set; }
+        
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType().FullName}: {this.Value}";
+        }
+    }
+
+}
---
+++ 5.GenericCountMethodDoubles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.GenericCountMethodDoubles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberofLines = int.Parse(Console.ReadLine());
+
+            var boxes = new List<Box<double>>();
+
+            for (int i = 0; i < numberofLines; i++)
+            {
+                var box = new Box<double>(double.Parse(Console.ReadLine()));
+                boxes.Add(box);
+            }
+
+            double elementToCompare = double.Parse(Console.ReadLine());
+
+            int count = GetCountOfGreaterElements(boxes, elementToCompare);
+            Console.WriteLine(count);
+            // Console.WriteLine(String.Join(Environment.NewLine, boxes));
+        }
+
+        private static int GetCountOfGreaterElements<T>(List<Box<T>> boxes, T elementToCompare)
+            where T : IComparable<T>
+        {
+            var count = boxes.Count(el => el.Value.CompareTo(elementToCompare) > 0);
+
+            return count;
+        }
+
+    }
+}
+
