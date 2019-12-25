bf48eb7d757d78a13cf810100750aee2702af863
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-25 13:56:00 +0100

    Problem 5. Generic Count Method Strings Solution

---
+++ 5.GenericCountMethodStirngs/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5.GenericCountMethodStrings
+{
+    public class Box<T>
+        where T: IComparable<T>
+    {
+
+        public T Value { get; set; }
+        public T Plamen { get; set; }
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType().FullName}: {this.Value}";
+        }
+    }
+}
---
+++ 5.GenericCountMethodStirngs/Program.cs
@@ -0,0 +1,38 @@
+﻿using _5.GenericCountMethodStrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.GenericCountMethodStirngs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberofLines = int.Parse(Console.ReadLine());
+
+            var boxes = new List<Box<string>>();
+
+            for (int i = 0; i < numberofLines; i++)
+            {
+                var box = new Box<string>(Console.ReadLine());
+                boxes.Add(box);
+            }
+
+            var elementToCompare = Console.ReadLine();
+
+            var count = GetCountOfGreaterElements(boxes, elementToCompare);
+            Console.WriteLine(count);
+            // Console.WriteLine(String.Join(Environment.NewLine, boxes));
+        }
+
+        private static int GetCountOfGreaterElements<T>(List<Box<T>> boxes, T elementToCompare)
+            where T : IComparable<T>
+        {
+            var count = boxes.Count(el => el.Value.CompareTo(elementToCompare) > 0);
+
+            return count;
+        }
+    }
+}
+
