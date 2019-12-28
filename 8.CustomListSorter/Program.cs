a506e3b4a7cfcce84d581c85e59a4a8bca64fbe1
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-28 23:25:34 +0100

    8.CustomListSorter Solution

---
+++ 8.CustomListSorter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _8.CustomListSorter
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var interpreter = new CommandInterpreter();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                interpreter.ParseCommand(input);
+            }
+
+        }
+    }
+}
---
+++ 8.CustomListSorter/Sorter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _8.CustomListSorter
+{
+   public class Sorter<T>
+        where T: IComparable<T>
+    {
+   public static CustomList<T> Sort(CustomList<T> list)
+        {
+            var sortedList = list
+                .GetList()
+                .OrderBy(x => x)
+                .ToList();
+
+            return new CustomList<T>(sortedList);
+        }
+
+    }
+}
