b33a8aebf95c42a726640d232d2e390bfc4cea7d
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2019-12-29 16:21:06 +0100

    11. Threeuple Solution

---
+++ 11.Threeuple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _11.Threeuple
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] firstLastAdress = Console.ReadLine().Split();
+            string firstName = firstLastAdress[0];
+            string lastName = firstLastAdress[1];
+            string address = firstLastAdress[2];
+            string town = firstLastAdress[3];
+            string name = firstName + " " + lastName;
+
+            var firstTuple = new Threeuple<string, string, string>(name, address, town);
+            Console.WriteLine(firstTuple);
+
+
+            string[] secondLine = Console.ReadLine().Split();
+            string nameSecondLine = secondLine[0];
+            int amountOfBeer = int.Parse(secondLine[1]);
+            string drunkOrNot = secondLine[2];
+
+            bool isDrunk = drunkOrNot == "drunk";
+
+            var secondTuple = new Threeuple<string, int, bool>(nameSecondLine, amountOfBeer, isDrunk);
+            Console.WriteLine(secondTuple);
+
+            string[] thirdLine = Console.ReadLine().Split();
+            string nameThirdLine = thirdLine[0];
+            double accountBalance = double.Parse(thirdLine[1]);
+            string bank = thirdLine[2];
+
+            var thirdTuple = new Threeuple<string, double, string>(nameThirdLine, accountBalance, bank);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ 11.Threeuple/Threeuple.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11.Threeuple
+{
+
+    public class Threeuple<T, U, P>
+    {
+        private T item1;
+        private U item2;
+        private P item3;
+
+        public P Item3
+        {
+            get { return item3; }
+            set { item3 = value; }
+        }
+
+
+        public T Item1
+        {
+            get { return item1; }
+            set { item1 = value; }
+        }
+
+        public U Item2
+        {
+            get { return item2; }
+            set { item2 = value; }
+        }
+
+
+        public Threeuple(T item1, U item2, P item3)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+
+        public override string ToString()
+        {
+            return $"{item1} -> {item2} -> {item3}";
+        }
+    }
+
+}
