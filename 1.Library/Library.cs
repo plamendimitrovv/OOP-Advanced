ec24e4c6007c1aefb440f17d2ea6a62fb74299c9
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2020-01-12 21:27:30 +0100

    1.Library solution

---
+++ 1.Library/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.Library
+{
+    public class Library
+    {
+        private List<Book> books { get; set; }
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerable<Book> GetEnumerator()
+        {
+            for (int i = 0; i < this.books.Count; i++)
+            {
+                yield return this.books[i]; 
+            }
+        }
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return GetEnumerator();
+        //}
+    }
+}
---
+++ 1.Library/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1.Library
+{
+    class Program
+    {
+        static void Main()
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+            Book bookFour = new Book("Once upon a time", 1990, "Some Author");
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+            Library libraryThree = new Library(bookFour);
+
+        }
+    }
+}
