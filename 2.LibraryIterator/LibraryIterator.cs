68b3dba91cbfe5f3d582809f57534ff39051e8df
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2020-01-19 17:30:44 +0100

    2. Library Iterator solution

---
+++ 2.LibraryIterator/LibraryIterator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2.LibraryIterator
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currentIndex;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.Reset();
+            this.books = new List<Book>(books);
+        }
+
+        public Book Current => this.books[this.currentIndex];
+
+        object IEnumerator.Current => this.Current; 
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            return ++this.currentIndex > this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.currentIndex = -1; 
+        }
+    }
+}
