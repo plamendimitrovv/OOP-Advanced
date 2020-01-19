d10cd29a1e6c3fa21ccd3bae186234b7d72b408f
plamendimitrovv <plamen.m.dimitrov@gmail.com>
2020-01-19 20:56:31 +0100

    4. Book Comparator solution

---
+++ 4.BookComparator/Library.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace _4.BookComparator
+{
+    public class Library: IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books, new BookComparator());
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
