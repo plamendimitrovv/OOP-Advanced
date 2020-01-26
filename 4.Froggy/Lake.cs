3422742e6f4651ae04877dff39d0768dd481e126
Plamen Dimitrov <plamen.dimitrov@ecx.io>
2020-01-26 21:27:31 +0100

    Problem 4. Froggy Solution

---
+++ 4.Froggy/Lake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        int[] stones;
+
+        public Lake(int[] stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Length; i += 2)
+            {
+                yield return this.stones[i];
+            }
+
+            int length = this.stones.Length;
+            int lastOddIndex = length % 2 == 0 ? length - 1 : length - 2;
+
+            for (int i = lastOddIndex; i > 0; i -= 2)
+            {
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 4.Froggy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] stones = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake lake = new Lake(stones);
+            Console.WriteLine(string.Join(", ", lake));
+            
+           
+
+
+        }
+    }
+}
