d6c04c89364e7d4a6b24f507636c60fe3822a28a
Plamen Dimitrov <plamen.dimitrov@ecx.io>
2020-01-27 22:21:52 +0100

    06. Strategy Pattern solution

---
+++ 6.StrategyPattern/PersonNameComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6.StrategyPattern
+{
+    public class PersonNameComparer : IComparer<Person>
+    {
+        public int Compare(Person firstPerson, Person secondPerson)
+        {
+            
+            int result = firstPerson.Name.Length.CompareTo(secondPerson.Name.Length);
+
+            if (result == 0)
+            {
+                char firstPersonLetter = char.ToLower(firstPerson.Name[0]);
+                char secondPersonLetter = char.ToLower(secondPerson.Name[0]); 
+
+                result = char.ToLower(firstPersonLetter).CompareTo(char.ToLower(secondPersonLetter)); 
+
+            }
+
+            return result; 
+        }
+    }
+}
